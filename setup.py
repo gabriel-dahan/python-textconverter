944a06925fb9e87ff3b6877e796284d7322f36dd
thegabdoosan <gabrieldahan05@gmail.com>
2021-09-08 16:37:31 +0200

    Changing github repo name in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version = "1.0",
     author = "Gabriel D.",
     description = "Make python read your text oraly (as a file or a simple string).",
-    url = "https://github.com/gabriel-dahan/textconverter",
+    url = "https://github.com/gabriel-dahan/text-converter",
     packages = setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
